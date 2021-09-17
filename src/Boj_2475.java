8be767105248f8ebcf62bd6bb546f8d194cb8b37
kim-soohyeon <kimsh1691@gmail.com>
2021-09-17 15:49:19 +0900

    2475 검증수

---
+++ src/Boj_2475.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj_2475 {
+
+	public static void main(String[] args) throws IOException{
+		//검증수
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		
+		int num;
+		int su = 0;
+		
+		for(int i = 0; i < 5; i++) {
+			num = Integer.parseInt(st.nextToken());
+			su += num * num;
+		}
+		
+		System.out.println(su % 10);
+		br.close();
+
+	}
+
+}
