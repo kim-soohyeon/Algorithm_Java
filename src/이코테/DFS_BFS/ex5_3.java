b73551f0bc576ee97c11007cd6792419bf0abf62
kim-soohyeon <kimsh1691@gmail.com>
2021-10-18 22:10:40 +0900

    재귀 함수 예제

---
+++ src/이코테/DFS_BFS/ex5_3.java
@@ -0,0 +1,14 @@
+package 이코테.DFS_BFS;
+
+public class ex5_3 {
+	//재귀 함수 예제
+	
+	public static void recursiveFunction() {
+        System.out.println("재귀 함수를 호출합니다.");
+        recursiveFunction();
+    }
+
+    public static void main(String[] args) {
+        recursiveFunction();
+    }
+}
