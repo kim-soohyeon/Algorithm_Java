d6b1a6a1293b69dc956e1974333b5bfc281b43cc
Soo Hyeon <kimsh1691@gmail.com>
2022-12-09 12:54:37 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 80 ms, Memory: 11620 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		for(int i = 1; i <= n; i++) {
+			for(int j = n - 1; j >= i; j--) {
+				sb.append(" ");
+			}
+			for(int j = 1; j <= (2 * i - 1); j++) {
+				sb.append("*");
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
+		
+		br.close();
+	}
+
+}
