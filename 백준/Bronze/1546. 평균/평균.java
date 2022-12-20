4d9fa8859ae33a7a90ca9431b9e0b9f47499ffa7
user <user@DESKTOP-K11VLKF>
2022-12-20 10:50:31 +0900

    평균 py -> java

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		float[] arr = new float[n];
+		float max = 0;
+		for(int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+			if(arr[i] > max) {
+				max = arr[i];
+			}
+		}
+		
+		float answer = 0;
+		for(int j = 0; j < n; j++) {
+			answer += (arr[j] / max * 100);
+		}
+		System.out.println(answer / n);
+		
+		br.close();
+	}
+}
