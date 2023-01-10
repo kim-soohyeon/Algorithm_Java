bbe1fe6c8f8c7e64d6968ad1160165729455b8b5
Soo Hyeon <kimsh1691@gmail.com>
2023-01-10 09:09:50 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 456 ms, Memory: 48324 KB -BaekjoonHub

---
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int t = Integer.parseInt(br.readLine()); // 테스트케이스 개수
+		int cnt; // 토큰 개수
+		ArrayList<String> arr;
+		StringTokenizer st;
+		for(int i = 0; i < t; i++) {
+			st = new StringTokenizer(br.readLine());
+			arr = new ArrayList<>();
+			
+			cnt = st.countTokens();
+			int arrLen;
+			for(int j = 0; j < cnt; j++) {
+				arr.add(st.nextToken());
+				arrLen = arr.get(j).length();
+				if(arrLen > 1) {
+					for(int k = arrLen - 1; k >= 0; k--) {
+						sb.append(arr.get(j).charAt(k));
+					}
+				}else {
+					sb.append(arr.get(j));
+				}
+				sb.append(" ");
+			}
+			sb.append("\n");
+		}
+		System.out.print(sb);
+
+		br.close();
+	}
+
+}
