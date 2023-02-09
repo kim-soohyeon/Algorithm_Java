d535725d7cea2c6d111b5e0054814d58206ceaaf
Soo Hyeon <kimsh1691@gmail.com>
2023-02-09 15:00:21 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.37 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.java
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -2,9 +2,8 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] array) {
-        int idx = (array.length / 2) + (array.length % 2) - 1;
         Arrays.sort(array);
-        int answer = array[idx];
+        int answer = array[array.length / 2];
         return answer;
     }
 }
