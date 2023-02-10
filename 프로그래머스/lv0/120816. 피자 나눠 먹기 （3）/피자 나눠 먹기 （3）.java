e3b0137f788c3f6e0f4040070603407231317e6e
Soo Hyeon <kimsh1691@gmail.com>
2023-02-10 14:36:31 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.02 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int slice, int n) {
+        int answer = 1;
+
+        while(slice * answer < n){
+            answer++;
+        }
+        
+        return answer;
+    }
+}
