e474cabf5ad8dff0fe606cf6e8c94ab09045f864
Soo Hyeon <kimsh1691@gmail.com>
2023-02-14 09:28:16 +0900

    [level 0] Title: 짝수의 합, Time: 0.02 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i = n; i >= 0; i--){
+            if(i % 2 == 0){
+                answer += i;
+            }
+        }
+        return answer;
+    }
+}
