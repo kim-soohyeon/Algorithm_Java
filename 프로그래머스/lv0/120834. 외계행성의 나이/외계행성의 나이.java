80461e11726c80fbf1a75d840ad99389c163029d
Soo Hyeon <kimsh1691@gmail.com>
2023-02-15 17:27:50 +0900

    [level 0] Title: 외계행성의 나이, Time: 12.76 ms, Memory: 71.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -1,24 +1,12 @@
 class Solution {
     public String solution(int age) {
         String answer = "";
+        String str_age = Integer.toString(age);
         
-        if(age < 10){
-            // 1자리
-            answer += Character.toString(age + 'a');
-        }else if(age < 100){
-            // 2자리
-            answer += Character.toString(age / 10 + 'a');
-            answer += Character.toString(age % 10 + 'a');
-        }else if(age < 1000){
-            // 3자리
-            answer += Character.toString(age / 100 + 'a');
-            answer += Character.toString(age % 100 / 10+ 'a');
-            answer += Character.toString(age % 10 + 'a');
-        }else{
-            // 4자리. 1000
-            answer = "baaa";
+        for(char chr : str_age.toCharArray()){
+            answer += (char)(chr - '0' + 'a');
         }
-
+        
         return answer;
     }
 }
