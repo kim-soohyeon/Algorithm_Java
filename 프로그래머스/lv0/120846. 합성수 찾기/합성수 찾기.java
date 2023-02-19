f80df57e4012cc3109c9028a232e941e17be7bfe
Soo Hyeon <kimsh1691@gmail.com>
2023-02-19 14:00:52 +0900

    [level 0] Title: 합성수 찾기, Time: 0.01 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i = 1; i <=n; i++){
+            int cnt = 0;
+            for(int j = 1; j <= i; j++){
+                if(i % j == 0){
+                    cnt++;
+                }
+            }
+            if(cnt >= 3){
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
