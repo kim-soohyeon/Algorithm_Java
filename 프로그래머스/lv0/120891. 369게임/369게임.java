3c6083ea34c3672163449e251a6ef0ed94c79e99
Soo Hyeon <kimsh1691@gmail.com>
2023-03-16 10:51:22 +0900

    [level 0] Title: 369게임, Time: 0.01 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.java
+++ 프로그래머스/lv0/120891. 369게임/369게임.java
@@ -2,17 +2,14 @@ import java.util.*;
 class Solution {
     public int solution(int order) {
         int answer = 0;
-        ArrayList<Integer> arrList = new ArrayList<>();
+        int num;
         
         while(order > 0){
-            arrList.add(order % 10);
-            order /= 10;
-        }
-        
-        for(int num : arrList){
+            num = order % 10;
             if(num == 3 || num == 6 || num == 9){
                 answer++;
             }
+            order /= 10;
         }
         
         return answer;
