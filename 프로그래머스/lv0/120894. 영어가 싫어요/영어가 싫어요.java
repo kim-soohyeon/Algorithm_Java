57397478ec5b2aed3396e84edfcfda05a36eed5b
Soo Hyeon <kimsh1691@gmail.com>
2023-03-17 09:27:50 +0900

    [level 0] Title: 영어가 싫어요, Time: 7.44 ms, Memory: 84.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public long solution(String numbers) {
+        long answer = 0;
+        
+        String[] strArr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        
+        for(int i = 0; i < strArr.length; i++){
+            if(numbers.contains(strArr[i])){
+                numbers = numbers.replace(strArr[i], i + ""); //치환
+            }
+            
+            try{
+                answer = Long.parseLong(numbers);
+                System.out.println(answer);
+                break;
+            }
+            catch (NumberFormatException ex){
+                continue;
+            }
+        }
+
+        return answer;
+    }
+}
