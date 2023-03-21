026c3df68fcd0c3b3e757633d702edff3c71940f
Soo Hyeon <kimsh1691@gmail.com>
2023-03-21 14:38:30 +0900

    [level 0] Title: 약수 구하기, Time: 0.38 ms, Memory: 79.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList<Integer> solution(int n) {
+        HashSet<Integer> hash = new HashSet<>();
+        for(int i = n; i > 0; i--){
+            if(n % i == 0){
+                hash.add(i);
+                hash.add(n / i);
+            }
+        }
+        
+        // HashSet -> ArrayList 변환 후 정렬
+        ArrayList<Integer> answer = new ArrayList<>(hash);
+        Collections.sort(answer);
+        
+        return answer;
+    }
+}
