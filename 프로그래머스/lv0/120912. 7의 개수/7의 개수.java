b67932e7490ebe8f0673248732c670523104ff25
Soo Hyeon <kimsh1691@gmail.com>
2023-03-24 10:37:28 +0900

    [level 0] Title: 7의 개수, Time: 0.02 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120912. 7의 개수/7의 개수.java
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.java
@@ -1,20 +1,16 @@
 class Solution {
     public int solution(int[] array) {
         int answer = 0;
+        
         for(int num : array){
-            if(num >= 10){
-                while(num > 0){
-                    if(num % 10 == 7){
-                        answer++;
-                    }
-                    num /= 10;
-                }
-            }else{
-                if(num == 7){
+            while(num > 0){
+                if(num % 10 == 7){
                     answer++;
                 }
+                num /= 10;
             }
         }
+        
         return answer;
     }
 }
--- 프로그래머스/lv0/120912. 7의 개수/README.md
+++ 프로그래머스/lv0/120912. 7의 개수/README.md
@@ -1,10 +1,10 @@
 # [level 0] 7의 개수 - 120912 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120912) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120912#) 
 
 ### 성능 요약
 
-메모리: 86.5 MB, 시간: 0.04 ms
+메모리: 72.8 MB, 시간: 0.02 ms
 
 ### 구분
 
