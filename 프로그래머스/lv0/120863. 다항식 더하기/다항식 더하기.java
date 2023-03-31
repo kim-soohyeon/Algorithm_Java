9a0b3e0c7001c66e20880719d194c833323c1a4d
Soo Hyeon <kimsh1691@gmail.com>
2023-03-31 09:58:30 +0900

    [level 0] Title: 다항식 더하기, Time: 1.99 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.java
+++ 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.java
@@ -2,25 +2,27 @@ class Solution {
     public String solution(String polynomial) {
         String answer = "";
         String[] arr = polynomial.split(" ");
-        int num1 = 0;
-        int num2 = 0;
-        
+        int num1 = 0;//일차항
+        int num2 = 0;//상수항
+
         for(int i = 0; i < arr.length; i += 2){
-            // System.out.println(arr[i]);
             if(arr[i].contains("x")){
+                //일차항
                 if(arr[i].length() > 1){
                     num1 += Integer.parseInt(arr[i].replace("x",""));
-                    // System.out.println(num);
                 }else{
                     num1++;
                 }       
             }else{
+                //상수항
                 num2 += Integer.parseInt(arr[i]);
             }
         }
-        
+
         if(num1 > 0){
+            //일차항이 있는 경우
             if(num1 > 1){
+                //계수 1은 생략
                 answer += Integer.toString(num1);
             }            
             answer += "x";
@@ -29,9 +31,10 @@ class Solution {
                 answer += Integer.toString(num2);
             }
         }else{
+            //일차항이 없는 경우
             answer += Integer.toString(num2);
         }
-        
+
         return answer;
     }
 }
