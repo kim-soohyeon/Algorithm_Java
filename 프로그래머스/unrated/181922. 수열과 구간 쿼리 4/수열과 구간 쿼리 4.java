e83e5e63720dc4b27adbdcc438e6965202812e2f
Soo Hyeon <kimsh1691@gmail.com>
2023-09-19 13:26:33 +0900

    [unrated] Title: 수열과 구간 쿼리 4, Time: 2.01 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = arr.clone();
+        for(int i = 0; i < queries.length; i ++){
+            for(int j = queries[i][0]; j <= queries[i][1]; j++){
+                if(j % queries[i][2] == 0){
+                    answer[j]++;
+                }
+            }
+        }
+        return answer;
+    }
+}
