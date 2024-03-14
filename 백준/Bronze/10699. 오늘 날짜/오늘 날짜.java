bf38ea84749640a96f8683a3fe9f898282eac2c2
Soo Hyeon <kimsh1691@gmail.com>
2024-03-14 17:11:53 +0900

    [Bronze V] Title: 오늘 날짜, Time: 112 ms, Memory: 13136 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,10 @@
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        // 현재 날짜 구하기
+        LocalDate now = LocalDate.now();
+        System.out.println(now);
+    }
+
+}
