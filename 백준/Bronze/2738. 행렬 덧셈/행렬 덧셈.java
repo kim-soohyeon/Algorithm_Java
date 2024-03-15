9df7a8d1476878d92c89aff7ddd5d267ab8b8d98
Soo Hyeon <kimsh1691@gmail.com>
2024-03-15 15:55:12 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 552 ms, Memory: 34236 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] a = new int[n][m];
+//        int[][] b = new int[n][m];
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                a[i][j] = sc.nextInt();
+            }
+        }
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                System.out.print(a[i][j] + sc.nextInt() + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
