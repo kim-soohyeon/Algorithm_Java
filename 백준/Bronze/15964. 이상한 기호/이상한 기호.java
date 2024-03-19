b086e76fa991a9c774d8fcd16b1dca6d599bae3a
Soo Hyeon <kimsh1691@gmail.com>
2024-03-19 12:48:59 +0900

    [Bronze V] Title: 이상한 기호, Time: 112 ms, Memory: 12984 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.java
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -3,8 +3,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        long a = sc.nextInt();
+        long b = sc.nextInt();
 
         System.out.println((a + b) * (a - b));
 
