bba076aff3f8c9723c3dd03ea6dabdf7d50f565b
Soo Hyeon <kimsh1691@gmail.com>
2024-11-05 13:13:01 +0900

    [level 0] Title: 배열의 평균값, Time: 6.18 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
@@ -2,7 +2,10 @@ import java.util.*;
 
 class Solution {
     public double solution(int[] numbers) {
-        double answer = Arrays.stream(numbers).asDoubleStream().average().getAsDouble();
+        double answer = Arrays.stream(numbers)
+            .asDoubleStream()
+            .average()
+            .orElse(0.0);
 
         return answer;
     }
