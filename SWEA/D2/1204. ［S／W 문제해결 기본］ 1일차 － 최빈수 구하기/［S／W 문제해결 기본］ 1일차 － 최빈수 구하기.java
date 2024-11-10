8116c41fdc87e44ea6faa8ddab390b08fdbcb1a8
Soo Hyeon <kimsh1691@gmail.com>
2024-11-11 00:05:49 +0900

    [D2] Title: [S/W 문제해결 기본] 1일차 - 최빈수 구하기, Time: 284 ms, Memory: 45,736 KB -BaekjoonHub

--- SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.java
+++ SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.java
@@ -7,19 +7,16 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         int T = sc.nextInt(); // 테스트 케이스 수
 
-        for (int test_case = 1; test_case <= T; test_case++) {
-            sc.nextInt(); // 테스트 케이스 번호는 읽고 넘어감
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            sc.nextInt();
             sb.append("#").append(test_case).append(" ");
             HashMap<Integer, Integer> map = new HashMap<>();
-
-            // 1000개의 점수 입력 받기
-            for (int i = 0; i < 1000; i++) {
-                int num = sc.nextInt(); // 각 점수를 입력받아 빈도수 카운트
+            for(int i = 0; i < 1000; i++){
+                int num = sc.nextInt();
                 map.put(num, map.getOrDefault(num, 0) + 1);
             }
-
-            // value 기준 내림차순, 같은 빈도수일 경우 점수 기준 내림차순
-            int answer = map.entrySet().stream()
+            int[] answer = map.entrySet().stream()
                     .sorted((o1, o2) -> {
                         int compare = Integer.compare(o2.getValue(), o1.getValue());
                         if (compare == 0) { // 빈도수가 같으면 점수 기준 내림차순
@@ -27,11 +24,8 @@ class Solution {
                         }
                         return compare;
                     })
-                    .mapToInt(HashMap.Entry::getKey)
-                    .findFirst()
-                    .orElse(0); // 결과가 없을 경우 기본값 0
-
-            sb.append(answer).append("\n");
+                    .mapToInt(HashMap.Entry::getKey).toArray();
+            sb.append(answer[0]).append("\n");
         }
         System.out.println(sb);
     }
