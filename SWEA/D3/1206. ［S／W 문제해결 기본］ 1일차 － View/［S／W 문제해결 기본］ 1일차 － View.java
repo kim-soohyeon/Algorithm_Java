ea1e8b21846d841a5743a912e2a4ec22759af786
Soo Hyeon <kimsh1691@gmail.com>
2024-11-11 16:27:23 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 188 ms, Memory: 36,784 KB -BaekjoonHub

--- SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.java
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.java
@@ -1,40 +1,32 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
-class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
-		Scanner sc = new Scanner(System.in);
+class Solution {
+    public static void main(String args[]) throws Exception {
+        Scanner sc = new Scanner(System.in);
         StringBuilder sb = new StringBuilder();
 
-		for(int test_case = 1; test_case <= 10; test_case++)
-		{
+        for (int test_case = 1; test_case <= 10; test_case++) {
             sb.append("#").append(test_case).append(" ");
             int N = sc.nextInt();
-            int arr[] = new int[N];
-            for(int i = 0; i < N; i++){
+            int[] arr = new int[N];
+            for (int i = 0; i < N; i++) {
                 arr[i] = sc.nextInt();
             }
 
             int answer = 0;
-            for(int i = 2; i < N - 2; i++) {
-                int newArr[] = new int[4];
-                if (N > 4) {
-                    newArr[0] = arr[i - 2];
-                    newArr[1] = arr[i - 1];
-                    newArr[2] = arr[i + 1];
-                    newArr[3] = arr[i + 2];
-
-                    Arrays.sort(newArr);
-
-                    if(arr[i] > newArr[3]){
-                        answer += arr[i] - newArr[3];
-                    }
+            for (int i = 2; i < N - 2; i++) {
+                // 현재 건물 기준으로 좌우 2칸만 비교
+                int leftMax = Math.max(arr[i - 2], arr[i - 1]);
+                int rightMax = Math.max(arr[i + 1], arr[i + 2]);
+                
+                // 현재 건물이 양쪽을 모두 초과하는 경우에만 조망권 확보
+                if (arr[i] > leftMax && arr[i] > rightMax) {
+                    answer += arr[i] - Math.max(leftMax, rightMax);  // 초과하는 높이만큼 더함
                 }
             }
+
             sb.append(answer).append("\n");
-		}
+        }
         System.out.println(sb);
-	}
+    }
 }
