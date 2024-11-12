a50ed64f992eff864090c7331e22c44467f8a479
Soo Hyeon <kimsh1691@gmail.com>
2024-11-12 23:53:15 +0900

    [D3] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 397 ms, Memory: 103,096 KB -BaekjoonHub

--- SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
+++ SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
@@ -9,26 +9,29 @@ class Solution
         StringBuilder sb = new StringBuilder();
         for(int test_case = 1; test_case <= 10; test_case++)
         {
-            sc.nextInt();
             sc.nextLine();
             sb.append("#").append(test_case).append(" ");
             int[] iSum = new int[100];
             int[] jSum = new int[100];
             int xSum = 0;
+            int ySum = 0;
             for(int i = 0; i < 100; i++){
                 for(int j = 0; j < 100; j++){
                     int num = sc.nextInt();
                     iSum[i] += num;
                     jSum[j] += num;
                     if(i == j) xSum += num;
+                    if(i + j == 99) ySum += num;
                 }
             }
             Arrays.sort(iSum);
             Arrays.sort(jSum);
 
-            int answer = Math.max(iSum[99], jSum[99]);
-            answer = Math.max(answer, xSum);
+            int max1 = Math.max(iSum[99], jSum[99]);
+            int max2 = Math.max(xSum, ySum);
+            int answer = Math.max(max1, max2);
             sb.append(answer).append("\n");
+            sc.nextLine();
         }
         System.out.println(sb);
     }
