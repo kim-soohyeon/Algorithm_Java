760ddf08ecbb184a842e6bd85de62848df622c83
Soo Hyeon <kimsh1691@gmail.com>
2024-11-22 14:51:18 +0900

    [level 2] Title: 올바른 괄호, Time: 6.18 ms, Memory: 52.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,24 +1,18 @@
-import java.util.*;
-
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
+        int count = 0;
         
-        if((s.charAt(0) == ')') || (s.charAt(s.length() - 1) == '(')) return false;
-        
-        Stack<Character> stack = new Stack<>();   
-        for(char chr : s.toCharArray()){
-            if(chr == '('){
-                stack.push(chr);
-            }else{
-                if(stack.size() > 0){
-                    stack.pop();
+        for (char chr : s.toCharArray()) {
+            if (chr == '(') {
+                count++;
+            } else {
+                count--;
+                if (count < 0) {
+                    return false;
                 }
             }
         }
         
-        if(stack.size() > 0) answer = false;
-
-        return answer;
+        return count == 0;
     }
 }
