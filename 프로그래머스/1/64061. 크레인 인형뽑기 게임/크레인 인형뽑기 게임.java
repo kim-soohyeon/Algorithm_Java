1c9b71e92515c0d2f5ca8cc2f539d9cd69e12553
Soo Hyeon <kimsh1691@gmail.com>
2024-11-29 16:54:17 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.46 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -1,28 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[][] board, int[] moves) {
         int answer = 0;
-        Stack<Integer> stack = new Stack<>();
-        for(int move : moves){
-            for(int i = 0; i < board[0].length; i++){
-                int num = board[i][move - 1];
-                if(num > 0){
-                    board[i][move - 1] = 0;
-                    if(stack.isEmpty()){
-                        stack.push(num);
-                    }else{
-                         if(num == stack.peek()){
-                            stack.pop();
-                            answer += 2;
-                        }else{
-                             stack.push(num);
-                         }
-                    }
-                    break;
-                }
-            }
-        }
         return answer;
     }
 }
