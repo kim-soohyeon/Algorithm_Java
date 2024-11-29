497721c41bd53ae2f1bf323de0982615356c8572
Soo Hyeon <kimsh1691@gmail.com>
2024-11-29 14:57:16 +0900

    [level 2] Title: 주식가격, Time: 43.43 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,17 +1,26 @@
+import java.util.Stack;
+
 class Solution {
     public int[] solution(int[] prices) {
-        int[] answer = new int[prices.length];
+        int n = prices.length;
+        int[] answer = new int[n];
+        
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+        for (int i = 1; i < n; i++) {
+            // 스택에 있는 값이 현재 값보다 크면 해당 주식 가격이 떨어진 시점을 계산
+            while (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
+                int index = stack.pop();
+                answer[index] = i - index;
+            }
+            // 스택에 현재 인덱스 추가
+            stack.push(i);
+        }
 
-        for(int i = 0; i < prices.length; i++){
-             int num1 = prices[i];
-             int cnt = 0;
-             for(int j = i + 1; j < prices.length; j++){
-                cnt++;
-                if(num1 > prices[j]) {
-                    break;
-                }
-             }
-             answer[i] = cnt;
+        // 아직 스택에 남아있는 값은 끝까지 가격이 떨어지지 않은 경우
+        while (!stack.isEmpty()) {
+            int index = stack.pop();
+            answer[index] = n - index - 1;
         }
 
         return answer;
