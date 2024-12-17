8781d115a6a8240fa7fdc972815fa44eea2ec6f1
Soo Hyeon <kimsh1691@gmail.com>
2024-12-17 16:58:29 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 58.98 ms, Memory: 113 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        Stack<Integer> stack = new Stack<>();
+
+        for(int num : arr){
+            if(stack.empty()){
+                stack.push(num);
+            }else if(stack.peek() != num){
+                stack.push(num);
+            }
+        }
+
+        int[] answer = new int[stack.size()];
+        for(int i = answer.length - 1; i >= 0; i--){
+            answer[i] = stack.pop();
+        }
+
+        return answer;
+    }
+}
