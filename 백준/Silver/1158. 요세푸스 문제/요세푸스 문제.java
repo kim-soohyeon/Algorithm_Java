f2b266c76f2875037e1ed705416d45abfa725b64
Soo Hyeon <kimsh1691@gmail.com>
2024-12-20 11:46:29 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 208 ms, Memory: 13684 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -3,36 +3,35 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-
-	public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
         Queue<Integer> queue = new ArrayDeque<>();
-        for(int i = 1; i <= n; i++){
+        for (int i = 1; i <= n; i++) {
             queue.add(i);
         }
 
         StringBuilder sb = new StringBuilder();
         sb.append("<");
-        int idx = 1;
-        while(!queue.isEmpty()){
-            if(queue.size() == 1){
-                sb.append(queue.poll());
-                break;
-            }
-            while(idx != k){
-                queue.add(queue.poll());
-                idx++;
-            }
-            sb.append(queue.poll() + ", ");
-            idx = 1;
-        }
-        sb.append(">");
 
+        int count = 0;
+        while (!queue.isEmpty()) {
+            count++;
+            if (count == k) {
+                sb.append(queue.poll());
+                count = 0;
+                if (!queue.isEmpty()) {
+                    sb.append(", ");
+                }
+            } else {
+                queue.add(queue.poll());
+            }
+        }
+
+        sb.append(">");
         System.out.println(sb);
-        br.close();
     }
-}    
+}
