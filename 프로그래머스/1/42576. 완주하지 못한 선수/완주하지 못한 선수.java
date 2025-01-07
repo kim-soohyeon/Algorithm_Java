948a88a360596e0b7568dd9d7e49555ef5f671db
Soo Hyeon <kimsh1691@gmail.com>
2025-01-07 13:30:10 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 73.55 ms, Memory: 97.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,22 +3,20 @@ import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         HashMap<String, Integer> hashmap = new HashMap<>();
-        for(String str : participant){
+        
+        // 완주자 배열을 먼저 HashMap에 추가
+        for (String str : completion) {
             hashmap.put(str, hashmap.getOrDefault(str, 0) + 1);
         }
 
-        for(String str : completion){
+        // 참가자 배열을 순회하며 값을 감소
+        for (String str : participant) {
+            if (hashmap.getOrDefault(str, 0) == 0) {
+                return str; // 완주하지 못한 선수
+            }            
             hashmap.put(str, hashmap.get(str) - 1);
         }
 
-        String answer = "";
-        for(String key: hashmap.keySet()){
-            if(hashmap.get(key) > 0){
-                answer = key;
-                break;
-            }
-        }
-
-        return answer;
+        return ""; // 모든 선수가 완주한 경우 (문제에서는 발생하지 않음)
     }
 }
