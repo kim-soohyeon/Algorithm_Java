554e74a20c2fb189ea74e5437fbc5bce3b3a4882
Soo Hyeon <kimsh1691@gmail.com>
2025-07-28 16:48:17 +0900

    [level 2] Title: 프로세스, Time: 1.07 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,31 +2,22 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] priorities, int location) {
-        Queue<Integer> priorityQueue = new LinkedList<>();
-        Queue<Integer> indexQueue = new LinkedList<>();
-
-        for (int i = 0; i < priorities.length; i++) {
-            priorityQueue.add(priorities[i]);
-            indexQueue.add(i);
-        }
-
         int answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
 
-        while (!priorityQueue.isEmpty()) {
-            int current = priorityQueue.poll();
-            int currentIndex = indexQueue.poll();
-
-            if (priorityQueue.stream().anyMatch(p -> p > current)) {
-                priorityQueue.add(current);
-                indexQueue.add(currentIndex);
-            } else {
-                answer++;
-                if (currentIndex == location) {
-                    return answer;
+        for(int num : priorities) {
+            pq.add(num);
+        }
+        while(!pq.isEmpty()) {
+            for(int i=0; i<priorities.length; i++) {
+                if(priorities[i] == pq.peek()) {
+                    pq.poll();
+                    answer++;
+                    if(i == location)
+                        return answer;
                 }
             }
         }
-
         return answer;
     }
 }
