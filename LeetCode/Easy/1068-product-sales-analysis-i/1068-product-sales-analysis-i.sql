6b6694df4b2d6dc457e00b5cbfd0eddebcc9c7e1
Soo Hyeon <kimsh1691@gmail.com>
2025-08-12 09:08:49 +0900

    Time: 816 ms (48.95%), Space: 0B (100%) - LeetHub

--- LeetCode/Easy/1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ LeetCode/Easy/1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -1,1 +1,8 @@
-# Write your MySQL query statement below
+/* Write your PL/SQL query statement below */
+SELECT PROD.product_name
+     , SAL.year
+     , SAL.price
+FROM Product PROD
+INNER JOIN Sales SAL
+    ON PROD.product_id = SAL.product_id
+;
